a72706d8b54517aacdc806829a456db315fa3646
Devanshi <gandhidevanshi99@gmail.com>
2022-04-04 12:58:11 -0400

    Initial

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.test1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,15 +19,18 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Spinner courselist;
-    TextView coursefees,coursehours,totalfees,totalhours;
+    TextView coursefees,coursehours,totalfees,totalhours,lab;
     EditText extStudent;
     Button btnSelect;
     RadioButton rdbOne,rdbTwo;
     CheckBox chb1,chb2;
     ArrayList<Course> cList=new ArrayList<>();
     ArrayList<String> cTypes=new ArrayList<>();
-    double originalFee;
-    int originalHour;
+    double tmptotfee=0,tmptothr=0,tohours=0,tofees=0,chours;
+    double chb1_val=0,chb2_val=0;
+    String flg="";
+    ArrayList<Course>tmpList=new ArrayList<>();
+    ArrayList<String>tmpNames=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,20 +41,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rdbOne.setOnClickListener(this);
         rdbTwo.setOnClickListener(this);
         btnSelect.setOnClickListener(this);
+
         if(rdbOne.isChecked())
         {
-            coursehours.setText("21");
+            flg="graduated";
         }
-
         ArrayAdapter aa=new ArrayAdapter(this, R.layout.spinnerdes,cTypes);
         courselist.setAdapter(aa);
         courselist.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                coursefees.setText(String.valueOf(cList.get(i).getCoursefees()));
-                originalFee=cList.get(i).getCoursefees();
+
+
                 coursehours.setText(String.valueOf(cList.get(i).getCoursehours()));
-                originalHour=cList.get(i).getCoursehours();
+                coursefees.setText(String.valueOf(cList.get(i).getCoursefees()));
+                tmptothr+=cList.get(i).getCoursehours();
+                tmptotfee+=cList.get(i).getCoursefees();
+                if (flg.equals("graduated")) {
+                    if (tmptothr > 21) {
+                        Log.d("test","dddd"+tmptothr);
+                    }
+                    else
+                    {
+                        tohours+=cList.get(i).getCoursehours();
+                        tofees+=cList.get(i).getCoursefees();
+                    }
+                } else if (flg.equals("ungraduated")) {
+                    if (tmptothr <= 19) {
+                        tohours+=cList.get(i).getCoursehours();
+                        tofees+=cList.get(i).getCoursefees();
+                    }
+                } else {
+                    chours = 0;
+                    tohours=0;
+                    tofees=0;
+                }
+
+                if(flg.equals("graduated") && tmptothr>21){
+                    lab.setText("Maximum Hours");
+                }
+                else if(flg.equals("ungraduated") && tmptothr>19){
+                    lab.setText("Maximum Hours");
+                }
+                else
+                {
+                    lab.setText("");
+                }
 
             }
 
@@ -85,35 +121,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rdbTwo=findViewById(R.id.rdbTwo);
         chb1=findViewById(R.id.chb1);
         chb2=findViewById(R.id.chb2);
+        lab=findViewById(R.id.lab);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.rdbOne:
-                coursehours.setText("21");
+                chours=21;
+                flg="graduated";
                 break;
             case R.id.rdbTwo:
-                coursehours.setText("19");
+                chours=19;
+                flg="ungraduated";
                 break;
             case R.id.btnSelect:
-                int chval = 0;
-                double Totalfees;
-                double fees = Double.parseDouble(coursefees.getText().toString());
-
-                        if (chb1.isChecked()) {
-                            chval = 1500;
+                for(Course prd:cList) {
+                    if (flg.equals("graduated")) {
+                        if (chours <= 21) {
+                            tmpList.add(prd);
+                            tmpNames.add(prd.getCoursename());
                         }
-
-                        if (chb2.isChecked()) {
-                            chval = 700;
-                        }
-
-                        Totalfees = fees + chval ;
-                        totalfees.setText(String.valueOf(totalfees));
-                        break;
                     }
+                    else if (flg.equals("ungraduated")) {
+                        if (chours <= 19) {
+                            tmpList.add(prd);
+                            tmpNames.add(prd.getCoursename());
+                        }
+                    }
+                    else {
+                        chours = 0;
+                    }
+                    //not working
+                  /*  if(tempNames.contains(prd.getCoursename()))
+                    {
+                        Toast.makeText(this,"Course is already selected",Toast.LENGTH_SHORT).show();
+                    }
+                  /*  obj=verifycourse(prd.getCoursename());
+                    if(obj==null)
+                    {
 
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"Course is already selected",Toast.LENGTH_SHORT).show();
+                    }*/
+
+                }
+                if (chb1.isChecked()) {
+                    chb1_val = 1000;
+                }
+                else
+                {
+                    chb1_val = 0;
+                }
+                if (chb2.isChecked()) {
+                    chb2_val = 700;
+                }
+                else
+                {
+                    chb2_val = 0;
+                }
+                totalhours.setText(String.valueOf(tmptothr));
+                totalfees.setText(String.valueOf(tmptotfee+chb1_val+chb2_val));
+                break;
+        }
                 }
 
 
@@ -123,4 +195,3 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-}
